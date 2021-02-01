29d2094921b4f7dd4d9725e2a3c0b22aed31cf6e
Sunkanmi-neulogic <sunkanmi.samson@neulogicsolutions.com>
2021-02-01 15:14:52 +0100

    first commit

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../index.js');
+
+describe ('GET /', function() {
+    it ('respond with Hello World', function(done) {
+        request(app).get('/').expect('Hello World!!', done);
+    });
+});
