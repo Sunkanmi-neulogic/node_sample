2d8cc83f8b09ac792008cb9c66d3d228cba92eb9
Sunkanmi-neulogic <sunkanmi.samson@neulogicsolutions.com>
2021-02-03 10:07:21 +0100

    3rd February, 10:09am

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require('../index.js');
 
 describe ('GET /', function() {
     it ('respond with Hello World', function(done) {
-        request(app).get('/').expect('Hello!!', done);
+        request(app).get('/').expect('Hello World!!', done);
     });
 });
