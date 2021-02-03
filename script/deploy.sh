655178cf99785301e4528802404be057346fa0ab
Sunkanmi-neulogic <sunkanmi.samson@neulogicsolutions.com>
2021-02-03 18:02:44 +0100

    testing deployment_part2

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh     
 ssh ubuntu@ip-172-31-20-90 <<EOF       
- cd /node_app 
- git pull https://github.com/Sunkanmi-neulogic/node_sample.git
+ cd ~/node_app 
+ git clone https://github.com/Sunkanmi-neulogic/node_sample.git
  npm install --production      
  pm2 restart all
  exit      
